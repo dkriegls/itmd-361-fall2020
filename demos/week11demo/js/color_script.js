1cac0c15317314ff40487fffc56a96b6197986d7
dkriegls <dkriegls@gmail.com>
2020-11-16 16:34:58 -0600

    Updating Demos

--- demos/week11demo/js/color_script.js
+++ demos/week11demo/js/color_script.js
@@ -4,7 +4,9 @@ function init(){
 		}
 
 		var d4 = document.getElementById('div4');
+    
 		d4.addEventListener('click', myEventFunction);
+    
 		d4.addEventListener('click', function(){
 			console.log('I was Clicked!');
 			d4.style.backgroundColor = '#C00';
